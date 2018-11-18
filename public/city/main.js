e231322a614f13073b3792800f9a0a2b4de3387f
reisub0 <reisub0@gmail.com>
2018-11-18 22:22:19 +0530

    Added city
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- public/city/main.js
+++ public/city/main.js
@@ -35,6 +35,51 @@
     }
   }
 
+  // generate the texture
+  var texture = new THREE.Texture(generateTexture());
+  texture.anisotropy = renderer.getMaxAnisotropy();
+  texture.needsUpdate = true;
+
+  // build the mesh
+  var material = new THREE.MeshLambertMaterial({
+    map: texture,
+    vertexColors: THREE.VertexColors
+  });
+
+  function generateTexture() {
+    // build a small canvas 32x64 and paint it in white
+    var canvas = document.createElement("canvas");
+    canvas.width = 32;
+    canvas.height = 64;
+    var context = canvas.getContext("2d");
+    // plain it in white
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 32, 64);
+    // draw the window rows - with a small noise to simulate light variations in each room
+    for (var y = 2; y < 64; y += 2) {
+      for (var x = 0; x < 32; x += 2) {
+        var value = Math.floor(Math.random() * 64);
+        context.fillStyle = "rgb(" + [value, value, value].join(",") + ")";
+        context.fillRect(x, y, 2, 1);
+      }
+    }
+
+    // build a bigger canvas and copy the small one in it
+    // This is a trick to upscale the texture without filtering
+    var canvas2 = document.createElement("canvas");
+    canvas2.width = 512;
+    canvas2.height = 1024;
+    var context = canvas2.getContext("2d");
+    // disable smoothing
+    context.imageSmoothingEnabled = false;
+    context.webkitImageSmoothingEnabled = false;
+    context.mozImageSmoothingEnabled = false;
+    // then draw the image
+    context.drawImage(canvas, 0, 0, canvas2.width, canvas2.height);
+    // return the just built canvas2
+    return canvas2;
+  }
+
   function animate(time) {
     renderer.setSize(window.innerWidth, window.innerHeight);
     if (controls.enabled) {
@@ -89,11 +134,24 @@
   }
 
   function buildings() {
+    // Make a new cube
     let geometry = new THREE.BoxGeometry(1, 1, 1);
+    // Let the cube's matrix be located at the bottom
     geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0));
+
+    // Remove bottom face as it is never seen
     let uv = geometry.faceVertexUvs[0];
     geometry.faces.splice(6, 2);
     uv.splice(6, 2);
+
+    // Set the top of the building to be the first pixel (white)
+    uv[4][0].set(0, 0);
+    uv[4][1].set(0, 0);
+    uv[4][2].set(0, 0);
+    uv[5][0].set(0, 0);
+    uv[5][1].set(0, 0);
+    uv[5][2].set(0, 0);
+    // geometry.faceVertexUvs[0][1][3].set(0, 0);
 
     let buildingMesh = new THREE.Mesh(geometry);
     let cityGeometry = new THREE.Geometry();
@@ -123,7 +181,53 @@
         }
       }
     }
-    let material = new THREE.MeshLambertMaterial({ color: 0x2ec184 });
+
+    // generate the texture
+    var texture = new THREE.Texture(generateTexture());
+    texture.anisotropy = renderer.getMaxAnisotropy();
+    texture.needsUpdate = true;
+
+    // build the mesh
+    var material = new THREE.MeshLambertMaterial({
+      map: texture,
+      vertexColors: THREE.VertexColors
+    });
+    var cityMesh = new THREE.Mesh(cityGeometry, material);
+
+    function generateTexture() {
+      // build a small canvas 32x64 and paint it in white
+      var canvas = document.createElement("canvas");
+      canvas.width = 32;
+      canvas.height = 64;
+      var context = canvas.getContext("2d");
+      // plain it in white
+      context.fillStyle = "#ffffff";
+      context.fillRect(0, 0, 32, 64);
+      // draw the window rows - with a small noise to simulate light variations in each room
+      for (var y = 2; y < 64; y += 2) {
+        for (var x = 0; x < 32; x += 2) {
+          var value = Math.floor(Math.random() * 64);
+          context.fillStyle = "rgb(" + [value, value, value].join(",") + ")";
+          context.fillRect(x, y, 2, 1);
+        }
+      }
+
+      // build a bigger canvas and copy the small one in it
+      // This is a trick to upscale the texture without filtering
+      var canvas2 = document.createElement("canvas");
+      canvas2.width = 512;
+      canvas2.height = 1024;
+      var context = canvas2.getContext("2d");
+      // disable smoothing
+      context.imageSmoothingEnabled = false;
+      context.webkitImageSmoothingEnabled = false;
+      context.mozImageSmoothingEnabled = false;
+      // then draw the image
+      context.drawImage(canvas, 0, 0, canvas2.width, canvas2.height);
+      // return the just built canvas2
+      return canvas2;
+    }
+    var material = new THREE.MeshLambertMaterial({ map: texture });
 
     var cityMesh = new THREE.Mesh(cityGeometry, material);
     return cityMesh;
